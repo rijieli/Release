1e6864bebdf4990114a664a838b7afc4c20a653a
Roger Lee <rijie.li@outlook.com>
2025-10-06 08:41:50 +0800

    Initial Commit

---
+++ Release/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Release
+//
+//  Created by Roger on 2025/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Release/ReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaseApp.swift
+//  Release
+//
+//  Created by Roger on 2025/10/6.
+//
+
+import SwiftUI
+
+@main
+struct ReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
